2cd18a73ba99a34e822a0eb978141bee609b5749
Dizza-code <aregeamos@gmail.com>
2025-08-17 00:26:12 +0100

    created api for crreation of accounts using parameters of owner name and currency

---
+++ api/account.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/Dizza-code/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	//input account into the database
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+	//call server to create account
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+// Get account by ID
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required"`
+}
---
+++ api/server.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	db "github.com/Dizza-code/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// This will create a new server instance and setup all api routes for our service on that server
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	//add routes to the router
+	router.POST("/accounts", server.createAccount)
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
